de0675f0c07793efc87c6dd0883c4f2737e5730a
wgheng <wgheng@outlook.com>
2017-07-06 16:58:52 +0800

    完成gift页面

--- app/src/main/java/com/wgheng/myapp/shop/fragment/GiftFragment.java
+++ app/src/main/java/com/wgheng/myapp/shop/fragment/GiftFragment.java
@@ -1,10 +1,14 @@
 package com.wgheng.myapp.shop.fragment;
 
-import android.view.Gravity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.wgheng.myapp.R;
 import com.wgheng.myapp.base.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * Created by wgheng on 2017/7/5.
@@ -12,12 +16,47 @@ import com.wgheng.myapp.base.BaseFragment;
 
 public class GiftFragment extends BaseFragment {
 
+    @BindView(R.id.iv_gift_best)
+    ImageView ivGiftBest;
+    @BindView(R.id.iv_festival)
+    ImageView ivFestival;
+    @BindView(R.id.iv_love)
+    ImageView ivLove;
+    @BindView(R.id.iv_birthday)
+    ImageView ivBirthday;
+    @BindView(R.id.iv_friend)
+    ImageView ivFriend;
+    @BindView(R.id.iv_child)
+    ImageView ivChild;
+    @BindView(R.id.iv_parent)
+    ImageView ivParent;
+    @BindView(R.id.ll_gift_remind)
+    LinearLayout llGiftRemind;
+
     @Override
     protected View initView() {
-        TextView textView = new TextView(getActivity());
-        textView.setTextSize(30);
-        textView.setGravity(Gravity.CENTER);
-        textView.setText("礼物");
-        return textView;
+        return View.inflate(getActivity(), R.layout.fragment_gift, null);
+    }
+
+    @OnClick({R.id.iv_gift_best, R.id.iv_festival, R.id.iv_love, R.id.iv_birthday, R.id.iv_friend, R.id.iv_child, R.id.iv_parent, R.id.ll_gift_remind})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.iv_gift_best:
+                break;
+            case R.id.iv_festival:
+                break;
+            case R.id.iv_love:
+                break;
+            case R.id.iv_birthday:
+                break;
+            case R.id.iv_friend:
+                break;
+            case R.id.iv_child:
+                break;
+            case R.id.iv_parent:
+                break;
+            case R.id.ll_gift_remind:
+                break;
+        }
     }
 }
